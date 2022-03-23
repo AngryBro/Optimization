3102066bd084a126ef77072c14e680813af35993
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-03-23 18:59:12 +1000

    matrix
    added invert, sum, dif, minot, A, minus

--- lab1.js
+++ lab1.js
@@ -1,5 +1,4 @@
 var body = document.body;
-var M = new Matrix('array',[[2,1],[0,1]]);
-var N = new Matrix('vector',[1,1]);
-var MN = M.mult(N);
-body.innerHTML += M.tex() + MN.tex();
+var M = new Matrix('array',[[2,5,4],[6,5,2],[6,1,1]]);
+var N = new Matrix('array',[[2,3,4],[4,5,5],[1,1,2]]);
+body.innerHTML += M.tex()+N.tex()+M.sum(N).tex()+M.tex()+N.tex()
--- script/matrix.js
+++ script/matrix.js
@@ -10,6 +10,9 @@ class Matrix {
 						}
 					}
 					this.array = arguments[1];
+					/*if(this.array.length*this.array[0].length==1) {
+						return this.array[0][0];
+					}*/
 				}
 				else {
 					return new None('Нет массива элементов матрицы.');
@@ -79,10 +82,21 @@ class Matrix {
 			sqr: this.array.length==this.array[0].length
 		};
 	}
+	copy() {
+		return new Matrix('array',JSON.parse(JSON.stringify(this.array)));
+	}
 	raw_tex() {
 		var temp = [];
+		var eps = (arguments.length>0)?arguments[0]:3;
+		var out = new Matrix('array',this.array.slice());
+		var p = Math.pow(10,eps);
 		for(var i = 0; i<this.size.n; i++) {
-			temp.push(this.array[i].join(' & '));
+			for(var j = 0; j<this.size.m;j++) {
+				out.array[i][j] = Math.round(out.array[i][j]*p)/p;
+			}
+		}
+		for(var i = 0; i<out.size.n; i++) {
+			temp.push(out.array[i].join(' & '));
 		}
 		return '\\begin{pmatrix} '+temp.join(' \\\\ ')+'\\end{pmatrix}';
 	}
@@ -94,6 +108,9 @@ class Matrix {
 		console.log(log.join('\n'));
 	}
 	tex() {
+		if(arguments.length>0) {
+			return this.T().raw_tex(arguments[0]);
+		}
 		return this.T().raw_tex();
 	}
 	log() {
@@ -106,7 +123,7 @@ class Matrix {
 		}
 		return this.array[j-1][i-1];
 	}
-	setElem(e,i,j) {
+	setElem(i,j,e) {
 		if((i<1)||(j<1)||(i>this.size.m)||(j>this.size.n)) {
 			console.log('Обращение к несущетсвующему элементу ('+i+','+j+')');
 		}
@@ -127,7 +144,7 @@ class Matrix {
 	}
 	mult(m_) {
 		if(m_==JSON.stringify(m_)) {
-			var arr = this.array;
+			var arr = JSON.parse(JSON.stringify(this.array));
 			for(var i = 0; i<arr.length; i++) {
 				for(var j = 0; j<arr[0].length; j++) {
 					arr[i][j] *= m_;
@@ -138,7 +155,7 @@ class Matrix {
 		else {
 			var _m_ = this.size.m;
 			var _n_ = this.size.n;
-			var _k_ = m_.size.m;
+			var _k_ = m_.size.n;
 			if(_n_!=m_.size.m) {
 				return new None('Перемножение несогласованных матриц');
 			}
@@ -150,12 +167,99 @@ class Matrix {
 						for(var k = 1; k<=_n_; k++) {
 							ij += this.getElem(i,k)*m_.getElem(k,j);
 						}
-						res.setElem(ij,i,j);
+						res.setElem(i,j,ij);
 					}
 				}
 				return res;
 			}
 		}
+	}
+	minor_matrix(_j,_i) {
+		var arr = [];
+		for(var i=0; i<this.size.n; i++) {
+			if(i!=_i-1) {
+				var temp = [];
+				for(var j = 0; j<this.size.m; j++) {
+					if(j!=_j-1) {
+						temp.push(this.array[i][j]);
+					}
+				}
+				arr.push(temp);
+			}
+		}
+		return new Matrix('array',arr);
+	}
+	det() {
+		if(!this.size.sqr) {
+			return new None('Попытка вычисления определителя не квадратной матрицы');
+		}
+		if(this.size.m*this.size.n==1) {
+			return this.array[0][0];
+		}
+		var s = 0;
+		for(var i = 0; i<this.size.m; i++) {
+			s+=this.array[0][i]*((i%2)*(-2)+1)*this.minor_matrix(i+1,1).det();
+		}
+		return s;
+	}
+	minor(i,j) {
+		return this.minor_matrix(i,j).det();
+	}
+	A(i,j) {
+		return (((i+j)%2)*(-2)+1)*this.minor(i,j);
+	}
+	invert() {
+		var res = new Matrix('0',this.size.m,this.size.n);
+		if(this.size.n!=this.size.m) {
+			return new None('Попытка обратить неквадратную матрицу');
+		}
+		var n = this.size.n;
+		for(var i = 1; i<=n; i++) {
+			for(var j = 1; j<=n; j++) {
+				res.setElem(j,i,this.A(i,j));
+			}
+		}
+		res = res.mult(1/this.det());
+		return res;
+	}
+	sum(s) {
+		var m = this.size.m;
+		var n = this.size.n;
+		if(JSON.stringify(s.size)!=JSON.stringify(this.size)) {
+			return new None('Матрицы разных размеров');
+		}
+		var res = this.copy();
+		for(var i = 0; i<n; i++) {
+			for(var j = 0; j<m; j++) {
+				res.array[i][j] += s.array[i][j];
+			}
+		}
+		return res;
+	}
+	dif(s) {
+		var m = this.size.m;
+		var n = this.size.n;
+		if(JSON.stringify(s.size)!=JSON.stringify(this.size)) {
+			return new None('Матрицы разных размеров');
+		}
+		var res = this.copy();
+		for(var i = 0; i<n; i++) {
+			for(var j = 0; j<m; j++) {
+				res.array[i][j] -= s.array[i][j];
+			}
+		}
+		return res;
+	}
+	minus() {
+		var m = this.size.m;
+		var n = this.size.n;
+		var res = this.copy();
+		for(var i = 0; i<n; i++) {
+			for(var j = 0; j<m; j++) {
+				res.array[i][j] *=(-1);
+			}
+		}
+		return res;
 	}
 }
 class None {
