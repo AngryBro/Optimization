64ad2346784347b1670fcbf85bd6c4a904951d34
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-03-23 19:00:36 +1000

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,4 +1,4 @@
 var body = document.body;
 var M = new Matrix('array',[[2,5,4],[6,5,2],[6,1,1]]);
 var N = new Matrix('array',[[2,3,4],[4,5,5],[1,1,2]]);
-body.innerHTML += M.tex()+N.tex()+M.sum(N).tex()+M.tex()+N.tex()
+body.innerHTML += M.tex()+N.tex()+M.dif(N).tex()+N.dif(M).tex()
